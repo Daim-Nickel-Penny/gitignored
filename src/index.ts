cf69bb76562d0d13b4e30353587a5a197ce60d3b
Daim-Nickel-Penny <1804449@kiit.ac.in>
2024-12-01 14:31:52 +0530

    Migrated Gitignore data

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { findGitIgnoreFile } from "./utils.js";
---
+++ src/utils.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import fs from "fs";
+
+/**Searches for a matching `.gitignore` file in the templates directory. */
+
+export const TEMPLATES_DIR = path.resolve(__dirname, "./templates");
+
+export const findGitIgnoreFile = (fileName: string) => {
+  try {
+    const allFiles = fs.readdirSync(TEMPLATES_DIR);
+    //search for <fileName>.gitignore
+    const matchedFile = allFiles.find(
+      (file) => file.includes(fileName) && file.endsWith(".gitignore")
+    );
+
+    if (matchedFile === undefined) {
+      throw new Error(
+        `Could not find a matching .gitignore file in the templates directory.`
+      );
+    }
+
+    return path.join(TEMPLATES_DIR, matchedFile);
+  } catch (e) {
+    throw new Error(
+      `Could not find a matching .gitignore file in the templates directory.`
+    );
+  }
+};
